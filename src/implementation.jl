e13226027ea13c08f9aa898b183ed50a41c0c145
James Cook <cookjws@gmail.com>
2018-09-24 20:05:57 +0100

    initial commit, tests pass

---
+++ src/implementation.jl
@@ -0,0 +1,93 @@
+const default_atol = eps()
+const default_rtol = sqrt(eps())
+
+function check_convergence(x_new::T, x_old::T,
+    rtol=default_rtol, atol=default_atol) where {T <: Number, N}
+  @assert !isnan(x_new) && !isnan(x_old) "x_new = $x_new, x_old = $x_old"
+  return isapprox(x_new, x_old, rtol=rtol, atol=atol)
+end
+function check_convergence(t_new::Array{T, N}, t_old::Array{T, N},
+    rtol=default_rtol, atol=default_atol) where {T <: Number, N}
+  @assert size(t_new) == size(t_old)
+  @inbounds for i ∈ eachindex(t_new)
+    !check_convergence(t_new[i], t_old[i], rtol, atol) && return false
+  end
+  return true
+end
+
+const default_initial_iterate = 5
+function _seriesaccelerator(accelerator::T, series::U,
+    sum_limit::V, recursion::Int, initial_iterate::W,
+    rtol, atol) where {T<:Function, U<:Function, V<:Int, W<:Int}
+  iterate = initial_iterate
+  recursion = Int(min(iterate - 1, recursion))
+  old_value = series(iterate) #accelerator(series, iterate, recursion)
+  isconverged = false
+  while !isconverged && iterate < sum_limit
+    iterate += 1
+    new_value = accelerator(series, iterate, recursion)
+    isfinite(new_value) || break
+    isconverged = check_convergence(new_value, old_value, rtol, atol)
+    old_value = deepcopy(new_value)
+  end
+  return old_value, isconverged
+end
+
+function _memoise(f::T, data::Dict=Dict()) where {T<:Function}
+  function fmemoised(i...)
+    !haskey(data, i) && (data[i] = f(i...))
+    return data[i]
+  end
+  return fmemoised, data
+end
+
+
+function shanks(series::T, sum_limit::U, recursion::Int;
+    rtol=default_rtol, atol=default_atol) where {T<:Function, U<:Int}
+  memoisedseries, data = _memoise(series)
+  f(n) = mapreduce(memoisedseries, +, 0:n)
+  return _seriesaccelerator(_shanks, f, sum_limit, recursion,
+    default_initial_iterate, rtol, atol)
+end
+
+function _shanks(f::T, sum_limit::U, recursion::Int) where {T<:Function, U<:Int}
+  @assert recursion >= 0 && sum_limit >= recursion
+  function _shanks_value(An1, An, An_1)
+    denominator = ((An1 - An) - (An - An_1))
+    iszero(denominator) && return An1 # then it's converged
+    return An1 - (An1 - An)^2/denominator
+  end
+  if recursion == 0
+    An1 = f(sum_limit + 1)
+    An = f(sum_limit + 0)
+    An_1 = f(sum_limit - 1)
+    return _shanks_value(An1, An, An_1)
+  else
+    An1 = _shanks(f, sum_limit + 1, recursion - 1)
+    An = _shanks(f, sum_limit, recursion - 1)
+    An_1 = _shanks(f, sum_limit - 1, recursion - 1)
+    return _shanks_value(An1, An, An_1)
+  end
+  throw("Shouldn't be able to reach here")
+end
+
+function vanwijngaarden(series::T, sum_limit::U, 
+    recursion::Int=default_initial_iterate;
+    rtol=default_rtol, atol=default_atol) where {T<:Function, U<:Int}
+  memoisedseries, data = _memoise(series)
+  return _seriesaccelerator(_vanwijngaarden, memoisedseries, sum_limit,
+    recursion, default_initial_iterate, rtol, atol)
+end
+
+function _vanwijngaarden(f::T, sum_limit::U, recursion::V
+                        ) where {T<:Function, U<:Int, V<:Int}
+  @assert sum_limit > 0 && recursion >= 0
+  if recursion == 0
+    return mapreduce(n -> f(n), +, 0:sum_limit)
+  else
+    return 0.5 * mapreduce(k -> _vanwijngaarden(f, k, recursion-1),
+      +, [1, -1] .+ sum_limit)
+  end
+  throw("Shouldn't be able to reach here")
+end
+
---
+++ test/run_tests.jl
@@ -0,0 +1,54 @@
+include("../src/SeriesAccelerators.jl")
+using Test, BenchmarkTools
+
+import Main: SeriesAccelerators
+
+@testset "Shanks" begin
+  summand(x, i) = Float64(x^i / factorial(BigInt(i)))
+  accelerator = SeriesAccelerators.shanks
+  @test exp(0.0) ≈ accelerator(i->summand(0.0, i), 1, 0)[1] rtol=sqrt(eps())
+  @test exp(0.0) ≈ accelerator(i->summand(0.0, i), 2, 0)[1] rtol=sqrt(eps())
+  @test exp(1.0) ≈ accelerator(i->summand(1.0, i), 10, 0)[1] rtol=sqrt(eps())
+  @test exp(1.0) ≈ accelerator(i->summand(1.0, i), 10, 1)[1] rtol=sqrt(eps())
+  x = -1.0
+  @test exp(x) ≈ accelerator(i->summand(x, i), 10, 0)[1] rtol=sqrt(eps())
+  @test exp(x) ≈ accelerator(i->summand(x, i), 10, 0)[1] rtol=sqrt(eps())
+  x = -2.0
+  @test exp(x) ≈ accelerator(i->summand(x, i), 20, 1)[1] rtol=sqrt(eps())
+  x = 2.0
+  @test exp(x) ≈ accelerator(i->summand(x, i), 15, 0)[1] rtol=sqrt(eps())
+end
+
+@testset "van Wijngaarden" begin
+  summand(x, i) = Float64(x^i / factorial(BigInt(i)))
+  accelerator = SeriesAccelerators.vanwijngaarden
+  for x = [0.0, 1.0, -1.0], c ∈ 1:5, j ∈ 1:5
+    i = c + j 
+    result = accelerator(i->summand(x, i), i, j)[1]
+    #@show x, i, j, exp(x), result
+  end
+
+  @test exp(0.0) ≈ accelerator(i->summand(0.0, i), 6, 5)[1] rtol=sqrt(eps())
+  @test exp(0.0) ≈ accelerator(i->summand(0.0, i), 7, 5)[1] rtol=sqrt(eps())
+  @test exp(1.0) ≈ accelerator(i->summand(1.0, i), 16, 8)[1] rtol=sqrt(eps())
+  x = -1.0
+  @test exp(x) ≈ accelerator(i->summand(x, i), 16, 8)[1] rtol=sqrt(eps())
+  @test exp(x) ≈ accelerator(i->summand(x, i), 32, 16)[1] rtol=sqrt(eps())
+  x = -2.0
+  @test exp(x) ≈ accelerator(i->summand(x, i), 20, 10)[1] rtol=sqrt(eps())
+  @test exp(x) ≈ accelerator(i->summand(x, i), 40, 20)[1] rtol=sqrt(eps())
+  x = 2.0
+  @test exp(x) ≈ accelerator(i->summand(x, i), 20, 10)[1] rtol=sqrt(eps())
+  @test exp(x) ≈ accelerator(i->summand(x, i), 40, 20)[1] rtol=sqrt(eps())
+end
+
+# time comparison
+summand(x, i) = Float64(x^i / factorial(BigInt(i)))
+@btime [SeriesAccelerators.shanks(i->summand(x, i), 20, 2)[1] for x ∈ (-5.0, -2.0, 0.0, 2.0, 5.0)]
+@btime [SeriesAccelerators.vanwijngaarden(i->summand(x, i), 20, 10)[1] for x ∈ (-5.0, -2.0, 0.0, 2.0, 5.0)]
+for x ∈ range(-10, stop=10, length=21)
+  ta = @elapsed a = SeriesAccelerators.shanks(i->summand(x, i), 20, 2)[1]
+  tb = @elapsed b = SeriesAccelerators.vanwijngaarden(i->summand(x, i), 20, 10)[1]
+  #r = exp(x)
+  #@show x, r, (a-r)/r,(b-r)/r, ta / tb
+end
